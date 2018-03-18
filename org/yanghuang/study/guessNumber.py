e98e832413fca9e49d14b9e7dff75a5b98a10c42
zhyhang <zhyhang1978@gmail.com>
2018-03-18 09:21:08 +0800

    code tidy

--- org/yanghuang/study/guessNumber.py
+++ org/yanghuang/study/guessNumber.py
@@ -9,20 +9,20 @@ Created on 2018年3月11日
 # guess the number through input
 import random
 
-secretNumber=random.randint(1,20)
+secretNumber = random.randint(1, 20)
 print('I am thinking of a number between 1 and 20.')
 
-for guessTaken in range(1,7):
+for guessTaken in range(1, 7):
     print('Take a guess')
-    guess=int(input())
+    guess = int(input())
     if guess < secretNumber:
         print('Your guess is too low.')
-    elif guess >secretNumber:
+    elif guess > secretNumber:
         print('Your guess is too high')
     else:
         break
-if guess==secretNumber:
-    print('Good job! You guess my number in '+str(guessTaken)+' guesses.')
+if guess == secretNumber:
+    print('Good job! You guess my number in ' + str(guessTaken) + ' guesses.')
 else:
-    print('Nope. The number I was thinking of was '+str(secretNumber)+'.')
+    print('Nope. The number I was thinking of was ' + str(secretNumber) + '.')
 
